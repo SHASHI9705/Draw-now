c4bfa4184e74f55dad47e981b041545673928c6c
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-04-08 23:23:38 +0530

    pp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey This is My first tym</h1>
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ()=>{
+    return <button>Click me more!1</button>
+}
+
+export default Button;
